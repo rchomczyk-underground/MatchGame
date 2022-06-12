59f5c030074c65e79b235100024ba39c1a86604c
Rafał Chomczyk <rchomczyk@icloud.com>
2022-06-12 16:11:02 +0200

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmojis = new List<string>()
+            {
+                "🦊", "🦊",
+                "🦍", "🦍",
+                "🦁", "🦁",
+                "🦝", "🦝",
+                "🦧", "🦧",
+                "🦄", "🦄",
+                "🦌", "🦌",
+                "🐗", "🐗",
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in gameGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmojis.Count);
+                string animalEmoji = animalEmojis[index];
+                animalEmojis.RemoveAt(index);
+                textBlock.Text = animalEmoji;
+            }
+        }
+    }
+}
